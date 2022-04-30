3729db91bb040dd32a3caaec945df5c72049f163
YaleXin <495592364@qq.com>
2022-04-30 22:46:48 +0800

    增加隐藏的超链接

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,6 +5,18 @@
 -->
 <template>
     <div>
+      <!-- 给搜索引擎看的 -->
+        <div hidden>
+          <ui>
+            <li><a href="home">首页</a></li>
+            <li><a href="category/-1">分类</a></li>
+            <li><a href="tag/-1">标签</a></li>
+            <li><a href="talk">吐槽</a></li>
+            <li><a href="link">友链</a></li>
+            <li><a href="archive">归档</a></li>
+          </ui>
+        </div>
+        <!-- Nuxt app 正文 -->
         <nuxt/>
     </div>
 </template>
